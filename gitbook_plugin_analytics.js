c3d41c355d59978ff0776658b8c669262ca756a4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-17 03:07:03 +0000

    Deploy pingcap-incubator/tidb-in-action to github.com/pingcap-incubator/tidb-in-action.git:gh-pages

--- gitbook_plugin_analytics.js
+++ gitbook_plugin_analytics.js
@@ -1,9 +1,27 @@
 if ('UA-160169257-1') {
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');		
 
-	ga('create', 'UA-160169257-1', 'auto');
-	ga('send', 'pageview');
+	ga('create', 'UA-160169257-1', 'auto', 'tracker');
+	ga('tracker.send', 'pageview');
+
+	let startState = window.history.state;
+	setInterval(function() {
+		let nowState = window.history.state;
+		if (!nowState) {
+			return;
+		}
+		if (!startState) {
+			startState = nowState;
+		}
+
+		if (startState.path !== nowState.path) {
+			ga('tracker.set', 'location', nowState.path);
+			ga('tracker.set', 'page', nowState.path);
+			ga('tracker.send', 'pageview');
+			startState = nowState;
+		}
+	}, 33)
 }
